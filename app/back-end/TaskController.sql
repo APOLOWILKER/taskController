cfdd2c6d1b73dbd9a21991ec4f60ce5459c0182b
APOLO WILKER <wilkerapolocarvalho@gmail.com>
2022-05-17 14:34:12 -0300

    :sparkles: feat: setup inicial

--- app/back-end/TaskController.sql
+++ app/back-end/TaskController.sql
@@ -4,12 +4,12 @@ CREATE DATABASE TaskController;
 
 USE TaskController;
 
-CREATE TABLE tasks (
+CREATE TABLE `tasks` (
     id INT NOT NULL auto_increment,
     titleTask VARCHAR(70) NOT NULL,
     contentTask VARCHAR(120) NOT NULL,
     statusTask VARCHAR(15) NOT NULL,
-) ENGINE=INNODB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 SET SQL_SAFE_UPDATES = 0;
 
