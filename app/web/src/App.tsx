a730efecadabff1a7aefab36f8fc978448ff8745
APOLO WILKER <wilkerapolocarvalho@gmail.com>
2022-05-20 01:15:45 -0300

    :sparkles: feat: iniciando o front-end

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -3,8 +3,8 @@ import './global.css'
 export function App() {
 
   return (
-    <div>
-      <h1>hello</h1>
+    <div className='flex  min-h-[600px] justify-center my-8 mx-6 border-solid border-2 border-sky-500 rounded-md'>
+      <h1 className='text-3xl font-semibold py-2'>My Tasks</h1>
     </div>
   )
 }
